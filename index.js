2a7922a7cf61085c482182127a91e2c5652612d6
phbelmiro <142456085+phbelmiro@users.noreply.github.com>
2023-08-21 21:44:04 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+*pedro henrique
+*primeiro comentario
+*/
+console.log("ola mundo")
+//prompt("Digite seu nome")
+
+//variaveis
+const variavelA = 100;
+const variavelB = variavelA + 50
+console.log(variavelB);
+
+let numero = 50
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+
+const nome = "pedro";
+const idade = 15;
+
+console.log("olá", "Meu nome é",nome, "e eu tenho", idade ,"anos");
+
+const sobrenome = "henrique";
+const estudante = "estudante";
+
+console.log("olá", "Meu nome é",nome,sobrenome, "e eu tenho", idade ,"anos", "é sou" ,estudante);
